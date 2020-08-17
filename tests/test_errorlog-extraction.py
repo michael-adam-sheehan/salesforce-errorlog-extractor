3abe769e074f3da710d31b5dd484fa2d48a5f4aa
Michael Sheehan <msheehan@Michaels-MacBook-Pro.local>
2020-08-16 21:25:31 -0700

    fixed tests; add cfg; upd gitignore

--- tests/test_errorlog-extraction.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from sfdc_errorlog_extractor.SFDCErrorLogExtractor import SFDCErrorLogExtractor
-
-def test_validate_good():
-    """Check whether correct JSON passes."""
-    assert True
